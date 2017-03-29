bcbc475dda6f892ca54025ba4c2ec168de4fd8d0
Marwen Trabelsi <marwen109@gmail.com>
2017-03-29 11:27:07 +0200

    fixed renamed file

--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,13 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 
-import App from './modules/App'
+import Nav from './modules/navPage'
 import Page1 from './modules/page1'
 import Page2 from './modules/page2'
 import NotFound from './modules/notfound'
 
 const routes = (
-  <Route path="/" component={App}>
+  <Route path="/" component={Nav}>
     <Route path="/page1" component={Page1}/>
     <Route path="/page2" component={Page2}/>
     <Route path="*" component={NotFound}/>
