1a3a09300f9aa36c6db6938c8f2393a466943526
Marwen Trabelsi <marwen109@gmail.com>
2017-03-29 10:12:20 +0200

    dropped hashHistory - fixed warning about signature

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {render} from 'react-dom'
-import { Router, Route , hashHistory} from 'react-router'
+import { Router, Route , browserHistory} from 'react-router'
 
 import App from './modules/App'
 import Page1 from './modules/page1'
@@ -9,7 +9,7 @@ import NotFound from './modules/notfound'
 
 
 render((
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
       <Route path="/" component={App}>
         <Route path="/page1" component={Page1}/>
         <Route path="/page2" component={Page2}/>
